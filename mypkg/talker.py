0c90a904f8393ef93eb5704db69afa8ca3de302f
Joffan Matthews <joffanmath22@gmail.com>
2022-12-01 14:01:03 +0900

    test.yml

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,16 +1,17 @@
-import rclpy                     #ROS2のクライアントのためのライブラリ
-from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-from person_msgs.srv import Query   #通信の型（16ビットの符号付き整数）
-
-def cb(request, response):
-    if request.name == "Joffan":
-        response.age = 44
-    else:
-        response.age = 255
-
-    return response
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
 
 rclpy.init()
-node = Node("talker") #ノード作成（nodeという「オブジェクト」を作成）
-srv = node.create_service(Query, "query", cb) #サービスの作成
-rclpy.spin(node)            #実行（無限ループ）def cb():          
+node = Node("talker")
+pub = node.create_publisher(Int16, "countup", 10)
+n = 0
+def cb():
+    global n
+    msg = Int16()
+    msg.data = n
+    pub.publish(msg)
+    n += 1
+
+node.create_timer(0.5, cb)
+rclpy.spin(node)
